469f771419d7f1bfb8d9d1215488130ee3b0571f
BastianSaavedra <bastiansaavedra55@gmail.com>
2024-07-29 00:02:26 -0400

    state

--- link_bio/link_bio.py
+++ link_bio/link_bio.py
@@ -4,8 +4,9 @@ import link_bio.styles.styles as styles
 from link_bio.pages.courses import courses
 from link_bio.pages.index import index
 
-# class State(rx.State):
-#     pass
+
+class State(rx.State):
+    pass
 
 
 app = rx.App(stylesheets=styles.STYLESHEETS, style=styles.BASE_STYLE)
